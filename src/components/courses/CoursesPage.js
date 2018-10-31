bf251384326f3bf154a3796c290156cb9f159bcb
Kevin Kane <kkane@tableau.com>
2018-10-31 16:59:58 -0700

    added thunk and loaded initial courses from mock api, added courseList display

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,67 +1,47 @@
-import React, {PropTypes} from "react";
+import React, { PropTypes } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import * as courseActions from '../../actions/courseActions';
+import * as courseActions from "../../actions/courseActions";
+import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
-    constructor(props, context){
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
+  }
 
-        this.state = {
-            course: { title: "" }
-        };
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
 
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
-
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);        
-    }
-
-    courseRow(course, index) {
-        return (<div key={index}>{course.title}</div>);
-    }
-
-    render(){
-        return (
-            <div>
-                <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input
-                    type="text"
-                    value={this.state.course.title} 
-                    onChange={this.onTitleChange} />
-                <input
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave} />
-            </div>
-        );
-    }
+  render() {
+    const { courses } = this.props;
+    return (
+      <div>
+        <h1>Courses</h1>
+        <CourseList courses={courses} />
+      </div>
+    );
+  }
 }
 
 CoursesPage.propTypes = {
-    courses: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
-    return {
-        courses: state.courses
-    };
+  return {
+    courses: state.courses
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators(courseActions, dispatch)
-    };
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
