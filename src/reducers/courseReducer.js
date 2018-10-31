85f72b8ebfd48e60234356fc12c0fa8207da42f2
Kevin Kane <kkane@tableau.com>
2018-10-31 16:18:17 -0700

    added constants for action types

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,9 +1,9 @@
+import * as types from "../actions/actionTypes";
 export default function courseReducer(state = [], action) {
-    switch(action.type) {
-        case 'CREATE_COURSE':
-            return [...state,
-                Object.assign({}, action.course)];
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case types.CREATE_COURSE:
+      return [...state, Object.assign({}, action.course)];
+    default:
+      return state;
+  }
 }
