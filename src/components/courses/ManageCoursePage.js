b2b23fcb333026c598903adadaf2ba5215532b7e
Kevin Kane <kevin@kevinjkane.com>
2018-11-03 12:20:43 -0700

    added author into reducers and made form editable

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as courseActions from '../../actions/courseActions';
+import * as authorActions from '../../actions/authorActions';
 import CourseForm from "./CourseForm";
 
 class ManageCoursePage extends React.Component {
@@ -11,12 +12,21 @@ class ManageCoursePage extends React.Component {
       course: Object.assign({}, this.props.course),
       errors: {}
     };
+    this.updateCourseState = this.updateCourseState.bind(this);
+  }
+
+  updateCourseState(event) {
+    const field = event.target.name;
+    let course = Object.assign({}, this.state.course);
+    course[field] = event.target.value;
+    return this.setState({course: course});
   }
 
   render() {
     return (
       <CourseForm 
-        allAuthors={[]}
+        allAuthors={this.props.authors}
+        onChange={this.updateCourseState}
         course={this.state.course}
         errors={this.state.errors}
       />
@@ -24,20 +34,28 @@ class ManageCoursePage extends React.Component {
   }
 }
 
-ManageCoursePage.PropTypes = {
-  course: PropTypes.object.isRequired
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+  const authorsFormattedForDropdown = state.authors.map(author => {
+    return {
+      value: author.id,
+      text: author.firstName + ' ' + author.lastName
+    };
+  });
   return {
-    course: course
+    course: course,
+    authors: authorsFormattedForDropdown
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
+    actions: bindActionCreators(courseActions, authorActions, dispatch)
   };
 }
 
