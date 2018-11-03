106ed4cef999b2df24bd4084720962ee52adfde5
Kevin Kane <kevin@kevinjkane.com>
2018-11-03 14:29:08 -0700

    added add course button

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -4,6 +4,14 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(savedCourse) {
+  return { type: types.CREATE_COURSE_SUCCESS, savedCourse };
+}
+
+export function updateCourseSuccess(updatedCourse) {
+  return { type: types.UPDATE_COURSE_SUCCESS, updatedCourse };
+}
+
 export function loadCourses() {
   return function(dispatch) {
     return courseApi
@@ -14,5 +22,18 @@ export function loadCourses() {
       .catch(error => {
         throw error;
       });
+  };
+}
+
+export function saveCourse() {
+  return function(dispatch, getState) {
+    return courseApi
+    .saveCourse()
+    .then(savedCourse => {
+      dispatch(createCourseSuccess(savedCourse));
+    })
+    .catch(error => {
+      throw error;
+    });
   };
 }
