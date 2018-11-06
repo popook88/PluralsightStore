3153ab8470c4aa7f13cd2fab9b8c4075260c8411
Kevin Kane <kkane@tableau.com>
2018-11-06 15:09:42 -0800

    added error handler

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -1,6 +1,13 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 
+function ajaxCallFinished(type) {
+  if (type == types.AJAX_CALL_ERROR || actionTypeEndsInSuccess(type)) {
+    return true;
+  }
+  return false;
+}
+
 function actionTypeEndsInSuccess(type) {
   return type.substring(type.length - 8) == "_SUCCESS";
 }
@@ -10,7 +17,7 @@ export default function ajaxStatusReducer(
 ) {
   if (action.type == types.BEGIN_AJAX_CALL) {
     return state + 1;
-  } else if (actionTypeEndsInSuccess(action.type)) {
+  } else if (ajaxCallFinished(action.type)) {
     return state - 1;
   }
   return state;
